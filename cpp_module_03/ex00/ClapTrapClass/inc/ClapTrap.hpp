72fe929962e3e6a4454cbf2f723b7bfc9c617d48
Cristóbal Peset Cruella <43825378+ElPatatin@users.noreply.github.com>
2023-11-02 14:30:27 +0000

    finished cpp03/ex00

--- cpp_module_03/ex00/ClapTrapClass/inc/ClapTrap.hpp
+++ cpp_module_03/ex00/ClapTrapClass/inc/ClapTrap.hpp
@@ -6,7 +6,49 @@
 /*   By: cpeset-c <cpeset-c@student.42barce.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 10:56:56 by cpeset-c          #+#    #+#             */
-/*   Updated: 2023/11/02 10:56:56 by cpeset-c         ###   ########.fr       */
+/*   Updated: 2023/11/02 14:05:41 by cpeset-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef _CLAPTRAP_HPP
+# define _CLAPTRAP_HPP
+
+# include <string>
+# include <iostream>
+# include <cstdlib>
+
+class ClapTrap
+{
+    private:
+        std::string Name;
+        unsigned int HitPoints;
+        unsigned int EnergyPoints;
+        unsigned int AttackDamage;
+
+    public:
+        // Constructor
+        ClapTrap(const std::string& name);
+        // Destructor
+        ~ClapTrap();
+
+        // Member function to attack a target
+        void attack(const std::string& target);
+        // Member function to take damage
+        void takeDamage(unsigned int amount);
+        // Member function to be repaired
+        void beRepaired(unsigned int amount);
+
+        // Setters
+        void setName( const std::string& name );
+        void setHitPoints( unsigned int hitPoints );
+        void setEnergyPoints( unsigned int energyPoints );
+        void setAttackDamage( unsigned int attackDamage );        
+
+        // Getters
+        std::string getName( void ) const;
+        unsigned int getHitPoints( void ) const;
+        unsigned int getEnergyPoints( void ) const;
+        unsigned int getAttackDamage( void ) const;
+};
+
+#endif
--- cpp_module_03/ex00/main.cpp
+++ cpp_module_03/ex00/main.cpp
@@ -6,7 +6,46 @@
 /*   By: cpeset-c <cpeset-c@student.42barce.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 10:54:48 by cpeset-c          #+#    #+#             */
-/*   Updated: 2023/11/02 10:54:49 by cpeset-c         ###   ########.fr       */
+/*   Updated: 2023/11/02 14:29:56 by cpeset-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <cassert>
+#include "ClapTrap.hpp"
+
+void test_ClapTrap_creation() {
+    ClapTrap claptrap("Test");
+    assert(claptrap.getName() == "Test");
+    // Add more assertions based on your ClapTrap class properties
+}
+
+void test_ClapTrap_attack() {
+    ClapTrap claptrap("Test");
+    claptrap.attack("Target");
+    // Replace expectedEnergyAfterAttack with the expected energy after attack
+    assert(claptrap.getEnergyPoints() == 9);
+}
+
+void test_ClapTrap_takeDamage() {
+    ClapTrap claptrap("Test");
+    claptrap.takeDamage(10);
+    // Replace expectedEnergyAfterDamage with the expected energy after taking damage
+    assert(claptrap.getEnergyPoints() == 10);
+    assert(claptrap.getHitPoints() == 0);
+    claptrap.takeDamage(10);
+}
+
+void test_ClapTrap_beRepaired() {
+    ClapTrap claptrap("Test");
+    claptrap.beRepaired(10);
+    // Replace expectedEnergyAfterRepair with the expected energy after being repaired
+    assert(claptrap.getEnergyPoints() == 9);
+}
+
+int main() {
+    test_ClapTrap_creation();
+    test_ClapTrap_attack();
+    test_ClapTrap_takeDamage();
+    test_ClapTrap_beRepaired();
+    return 0;
+}
