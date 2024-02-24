bb182ff4e39dddc9f6a0f847d504371fae2ae189
ElPatatin <cristobalpes1@gmail.com>
2024-02-24 17:28:55 +0100

    Started prep for cpp04/ex03

---
+++ cpp_module_04/ex04/CharacterClass/Character.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Character.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cpeset-c <cpeset-c@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/24 17:26:24 by cpeset-c          #+#    #+#             */
+/*   Updated: 2024/02/24 17:26:35 by cpeset-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CHARACTER_HPP
+# define CHARACTER_HPP
+
+# include <iostream>
+
+class Character
+{
+	public:
+
+	private:
+
+};
+
+#endif
