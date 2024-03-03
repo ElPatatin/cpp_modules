4f66da88e4a8d32beb13da49d5c711ccc1cbcf63
Cristóbal Peset Cruella <43825378+ElPatatin@users.noreply.github.com>
2024-03-03 20:21:30 +0100

    started cpp09/ex00

---
+++ cpp_module_09/ex00/BitcoinExchageClass/BitcoinExchange.hpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cpeset-c <cpeset-c@student.42barce.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/03 20:12:36 by cpeset-c          #+#    #+#             */
+/*   Updated: 2024/03/03 20:12:53 by cpeset-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
+
+# include <iostream>
+# include <string>
+# include <iomanip>
+
+#endif
