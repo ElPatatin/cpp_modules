11bf654c673fee40f07a48233a0105ad1f00283c
ElPatatin <cristobalpes1@gmail.com>
2024-03-04 14:47:18 +0100

    some progress on cpp09

--- cpp_module_09/ex00/BitcoinExchageClass/BitcoinExchange.hpp
+++ cpp_module_09/ex00/BitcoinExchageClass/BitcoinExchange.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cpeset-c <cpeset-c@student.42barce.com>    +#+  +:+       +#+        */
+/*   By: cpeset-c <cpeset-c@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/03 20:12:36 by cpeset-c          #+#    #+#             */
-/*   Updated: 2024/03/03 20:12:53 by cpeset-c         ###   ########.fr       */
+/*   Updated: 2024/03/04 13:28:27 by cpeset-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,43 @@
 # include <iostream>
 # include <string>
 # include <iomanip>
+# include <list>
 
-#endif
+class BitcoinExchange
+{
+    public:
+        // CONSTRUCTOR AND DESTRUCTOR
+        // ==========================
+
+        BitcoinExchange();
+        BitcoinExchange( BitcoinExchange const & src );
+        ~BitcoinExchange();
+
+        // OPERATOR OVERLOAD
+        // =================
+
+        BitcoinExchange &operator=( BitcoinExchange const & rhs );
+
+        // CONSTRUCTOR WITH PARAMS
+        // =======================
+    
+        BitcoinExchange( std::string const &name, double const &rate );
+
+        // MEMBER FUNCTIONS
+        // ================
+
+        // GETTERS AND SETTERS
+        // ===================
+
+        std::string const   &getName() const;
+        double const        &getRate() const;
+
+        void                setName(std::string const &name);
+        void                setRate(double const &rate);
+
+    private:
+        std::string _name;
+        double _rate;
+};
+
+#endif /* *********************************************** BITCOINEXCHANGE_HPP */
