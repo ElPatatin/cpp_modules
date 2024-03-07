a4c810342b49f6b810e07cfb300bcc057a35d82b
Cristóbal Peset Cruella <43825378+ElPatatin@users.noreply.github.com>
2024-03-07 17:30:21 +0100

    started cpp09/ex02

---
+++ cpp_module_09/ex02/PmergeMeClass/PmergeMe.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cpeset-c <cpeset-c@student.42barce.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/07 17:21:29 by cpeset-c          #+#    #+#             */
+/*   Updated: 2024/03/07 17:28:39 by cpeset-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PMERGEME_HPP
+# define PMERGEME_HPP
+
+# include <iostream>
+# include <string>
+# include <deque>
+# include <list>
+
+// COLORS
+// ======
+
+# define RED     "\033[31m"
+# define GREEN   "\033[32m"
+# define YELLOW  "\033[33m"
+# define BLUE    "\033[34m"
+# define MAGENTA "\033[35m"
+# define CYAN    "\033[36m"
+# define WHITE   "\033[37m"
+# define ORANGE  "\033[38;5;208m"
+# define DEFAULT "\033[0m"
+
+// CONSTANTS
+// =========
+
+# define UNUSED( x )    ( void )x
+
+#endif
